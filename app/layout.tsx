7da36c30a79f4ceb0e6c43d514347b156388465c
fattahsmith <fattahsmith08@gmail.com>
2026-02-04 11:54:38 +0700

    chore: upgrade Next.js & React to stable versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { StaffAuthProvider } from "@/contexts/StaffAuthContext";
-import Image from "next/image";
-
-
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
